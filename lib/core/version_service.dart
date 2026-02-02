9dd05ccd26cfd357390624a68bf96bb7687fddc8
shimkijoon <skjoon@gmail.com>
2026-02-03 00:21:05 +0900

    fix: VersionService HardwareKeyboard import and bump to v1.0.16

--- lib/core/version_service.dart
+++ lib/core/version_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider/path_provider.dart';
